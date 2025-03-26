aeb024dc57065763ea0f38b7032cdb6bc4c2209e
Farhan Nadim Iqbal <nadeemomi321@hotmail.com>
2025-03-26 06:33:53 +0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,70 @@
+/*
+Project Name: Array Manipulation
+Creation Date: Monday, March 24, 2025
+Created By: Farhan Nadim Iqbal
+Student ID: 0182420012101240
+Course Instructor: Md. Jehadul Islam Mony
+Batch: CSE 64F
+Department: Computer Science and Engineering
+Institution: Leading University, Sylhet
+*/
+
+#include <stdio.h>
+
+//--> Function Declarations
+void display();
+void info();
+
+//--> Main Code
+int main()
+{
+    //--> Specifying the size of the array
+    int size = 0;
+    int array[size];
+
+
+
+
+
+    return 0;
+}
+
+//--> Function Logic
+void info()
+{
+    printf("Welcome to Array Manipulation Program.");
+    printf("\n\n");
+    printf("Usage Guide:");
+    printf("\n");
+    printf("You will first have to specify the number of elements you will enter. 0 is considered a blank value.");
+    printf("\n");
+    printf("Then you will be given several options to choose from.");
+    printf("\n");
+
+}
+void display()
+{
+    printf("Options: ");
+    printf("\n");
+    printf("1. Insert Values");
+    printf("\n");
+    printf("2. Update Values");
+    printf("\n");
+    printf("3. Delete Values");
+    printf("\n");
+    printf("4. Display Values");
+    printf("\n");
+    printf("5. Display Array Size");
+    printf("\n");
+    printf("6. Search Values");
+    printf("\n");
+    printf("7. Smallest Value");
+    printf("\n");
+    printf("8. Largest Value");
+    printf("\n");
+    printf("9. Sort Array (Ascending/Descending)");
+    printf("\n");
+    printf("10. Exit");
+    printf("\n\n");
+
+}
